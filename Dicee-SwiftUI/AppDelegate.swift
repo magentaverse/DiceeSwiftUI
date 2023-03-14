44053d3943bde9da47658b513fa3a0240c024d87
Joanna Szaban <joannaszaban@MacBook-Pro-Joanna.local>
2023-03-14 12:13:33 +0100

    first commit

---
+++ Dicee-SwiftUI/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  Dicee
+//
+//  Created by Joanna Szaban on 17/01/2023.
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+      
+        return true
+    }
+
+   
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+       
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+       
+    }
+
+
+}
+
